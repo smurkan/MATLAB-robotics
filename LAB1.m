41117780807d5cd4672de4a56ef486877de7561c
smurkan <robert.glaes96@gmail.com>
2018-12-04 10:56:17 +0100

    LAB1 done

--- LAB1.m
+++ LAB1.m
@@ -7,12 +7,13 @@ T = transl([2 0 0]);
 p0 = [0 0 0 1];
 pc = T * p0';
 pc'
-%% HM2
+%% HM2 - plotting position of end-effector
 T=transl([-0.5 1.5 0]);
 % for config elbow up
 % q = r2dp.ikine(T,'mask',[1 1 0 0 0 0])
 % for config elbow down
 q = r2dp.ikine(T,'mask',[-1 1 0 0 0 0])
+%plots
 r2dp.plot(q,'view','top','tilesize',3,'workspace',[-5 5 -3 3 -1 1]);
 figure
 scatter(-0.5,1.5,'filled')
@@ -35,7 +36,7 @@ q = r2dp.ikine(pe, 'mask', [1 1 0 0 0 0])
 pc = pe.T * [0 0 0 1]';
 pc'
 
-%% HM3
+%% HM3 - Rotation and translation of pe
 %converts homogeneous transform to rotation and translation
 [R,t] = tr2rt(pe)
 tr2angvec(R)
@@ -58,15 +59,16 @@ r2dp.plot(q,'view','top','tilesize',3,'workspace',[-5 5 -3 3 -1 1],'trail',{'r',
 T0 = transl([2 0 0]);
 T1 = transl([0 1 0]);
 Ts = ctraj(T0, T1, 20);
+
 qs = r2dp.ikine(Ts, 'mask', [1 1 0 0 0 0]);
-%qs = r2dp.ikine(Ts);
+
 r2dp.plot(qs,'view','top','tilesize',3,'workspace',[-5 5 -3 3 -1 1],'trail',{'b','linewidth',10})
 dx = linspace(1,20,20);
 figure;
 plot(dx,qs)
-%% HM4
-%[q,qd,qdd] =jtraj([0 r2dp.ikine(T0,'mask',[2 0 0 0 0 0])],[0 r2dp.ikine(T1,'mask',[0 1 0 0 0 0])],20);
+%% HM4 - inverse kinematics w/ joint limits
 qs2 = r2dp.ikcon(Ts);
+%plots
 r2dp.plot(qs2,'view','top','tilesize',3,'workspace',[-5 5 -3 3 -1 1],'trail',{'b','linewidth',10})
 figure;
 plot(dx,qs2)
@@ -76,7 +78,8 @@ ylabel('Joint Angles')
 xlim([0 20])
 ylim([-1 2.5])
 %% Part 4
-r2dp.gravity = [0;9.81;0]
+% gravity in the y-direction
+r2dp.gravity = [0;9.81;0];
 
 r2dp.links(1,1).m = 50;
 r2dp.links(1,1).r = [-0.5,0,0];
@@ -94,9 +97,7 @@ r2dp.links(1,2).dyn
 
 % Generate a trajectory between two positions
 [q,qd,qdd] = jtraj([pi/4 0],[pi/4 pi/2],20);
-%r2dp.plot(q,'view','top','tilesize',3,'workspace',[-5 5 -3 3 -1 1],'trail',{'b','linewidth',10})
-r2dp.plot([pi/4 pi/3],'jvec')
-figure
+
 % Calculate the joints torques required to achieve
 % the generated positions
 Q = r2dp.rne(q,qd,qdd);
@@ -112,18 +113,11 @@ subplot(212)
 plot(q(:,1))
 hold on
 plot(q(:,2),'r')
-
 ylabel('Joints Angles')
 legend('q1','q2')
 xlabel('Trajectory Steps')
-% subplot(314)
-% plot(qdd(:,1))
-% hold on
-% plot(qdd(:,2),'r')
-% ylabel('Joints acc')
-% legend('q1','q2')
-% xlabel('Trajectory Steps')
-%% HM 6
+
+%% HM6 - Changing m2, plot changes
 r2dp.links(1,2).m=50;
 for i=[50 100 150]
     r2dp.links(1,1).m = i;
@@ -136,7 +130,7 @@ end
 plot(Q(:,2),'r')
 legend('J1, m1=50','J1, m1=100','J1, m1=150','J2')
 
-%%
+%% HM6 - Changing m1, plot changes
 r2dp.links(1,1).m=50;
 for i=[50 100 150]
     r2dp.links(1,2).m = i;
@@ -147,9 +141,8 @@ for i=[50 100 150]
     ylabel('Joints Torques')
     xlabel('Trajectory Steps')
 end
-%plot(Q(:,2),'r')
 legend('J1, m2=50','J2, m2=50','J1, m2=100','J2, m2=100','J1, m2=150','J2, m2=150')
-%% HM7
+%% HM7 - Calculate the gravity load and plot against joint torque
 Grav = r2dp.gravload(q);
 plot(Q(:,1))
 hold on
